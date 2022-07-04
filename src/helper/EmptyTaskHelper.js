150ef66a583e21b54a8881a3de2cf17150dc1a6a
rexcyrio <stefancyrio@gmail.com>
2022-07-04 10:57:14 +0800

    refactor: logic migration to redux (part 5)

---
+++ src/helper/EmptyTaskHelper.js
@@ -0,0 +1,16 @@
+export const EMPTY_TASK = {
+  _id: "0",
+
+  name: "",
+  dueDate: "",
+  dueTime: "",
+  durationHours: "",
+  moduleCode: "",
+  links: [],
+
+  row: -1,
+  col: -1,
+  timeUnits: 1,
+
+  isCompleted: false,
+};
--- src/pages/Private.js
+++ src/pages/Private.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import MyDragLayer from "../components/helperComponents/MyDragLayer";
 import Links from "../components/links/Links";
 import Notes from "../components/notes/Notes";
-import Scheduler from "../components/scheduler/Scheduler";
+import Tasks from "../components/tasks/Tasks";
+import Timetable from "../components/timetable/Timetable";
 
 function Private() {
   return (
     <>
       <DndProvider backend={HTML5Backend}>
+        <MyDragLayer />
         <div className="background">
           <div className="grid">
             <div className="links-section">
@@ -17,7 +20,12 @@ function Private() {
             <div className="notes-section">
               <Notes />
             </div>
-            <Scheduler />
+            <div className="tasks-section">
+              <Tasks />
+            </div>
+            <div className="timetable-section">
+              <Timetable />
+            </div>
           </div>
         </div>
       </DndProvider>
