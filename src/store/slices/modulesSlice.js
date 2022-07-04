6bf1cbbb9446c9a6ad066d22f340bcde6513b539
wz2k <87819279+wz2k@users.noreply.github.com>
2022-07-04 10:08:48 +0800

    refactor: logic migration to redux (part 3)

---
+++ src/store/slices/modulesSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const modulesSlice = createSlice({
+  name: "modules",
+  initialState,
+  reducers: {},
+});
+
+export default modulesSlice.reducer;
