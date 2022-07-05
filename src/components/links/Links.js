15a96604b35b7237228f5bb1e04efdad6257db62
wz2k <87819279+wz2k@users.noreply.github.com>
2022-07-05 21:12:32 +0800

    refactor: seperate links editor

--- src/components/links/Links.js
+++ src/components/links/Links.js
@@ -1,8 +1,8 @@
 import AddIcon from "@mui/icons-material/Add";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import DeleteIcon from "@mui/icons-material/Delete";
-import EditIcon from "@mui/icons-material/Edit";
-import RestoreIcon from "@mui/icons-material/Restore";
+
+
+
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
@@ -20,11 +20,8 @@ import { Alert } from "@mui/material";
 import DataStatus from "../helperComponents/DataStatus";
 import LinksList from "./LinksList";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  addNewPermLink,
-  fetchPermLinks,
-  saveEditedPermLinks,
-} from "../../store/slices/linksSlice";
+import { addNewPermLink, fetchPermLinks } from "../../store/slices/linksSlice";
+import LinksEditor from "./LinksEditor";
 
 function Links() {
   const dispatch = useDispatch();
@@ -33,8 +30,7 @@ function Links() {
   const [newName, setNewName] = useState("");
   const [newURL, setNewURL] = useState("");
   const [add_open, add_setOpen] = useState(false);
-  const [edit_open, edit_setOpen] = useState(false);
-  const [tempLinks, setTempLinks] = useState([]);
+
   const [openSyncErrorSnackbar, setOpenSyncErrorSnackbar] = useState(false);
 
   useEffect(() => {
@@ -56,55 +52,6 @@ function Links() {
     setNewName("");
     setNewURL("");
   };
-
-  const edit_openDialog = () => {
-    if (links.length > 0) {
-      setTempLinks([...links]);
-      edit_setOpen(true);
-    }
-  };
-
-  const edit_closeDialog = (saveChanges) => {
-    edit_setOpen(false);
-
-    if (saveChanges) {
-      dispatch(saveEditedPermLinks(tempLinks));
-    }
-  };
-
-  function updateTempLink(newLink) {
-    setTempLinks((prev) => {
-      const index = prev.findIndex((each) => each._id === newLink._id);
-      return [...prev.slice(0, index), newLink, ...prev.slice(index + 1)];
-    });
-  }
-
-  function toggleToBeDeleted(self) {
-    const newLink = {
-      ...self,
-      _toBeDeleted: !self._toBeDeleted,
-    };
-
-    updateTempLink(newLink);
-  }
-
-  function updateTempName(self, name) {
-    const newLink = {
-      ...self,
-      name: name,
-    };
-
-    updateTempLink(newLink);
-  }
-
-  function updateTempURL(self, url) {
-    const newLink = {
-      ...self,
-      url: url,
-    };
-
-    updateTempLink(newLink);
-  }
 
   function addNewLink() {
     dispatch(addNewPermLink(newURL, newName));
@@ -131,11 +78,7 @@ function Links() {
           <DataStatus status={status} />
         </div>
         <div>
-          <Tooltip title="Edit">
-            <IconButton onClick={edit_openDialog}>
-              <EditIcon />
-            </IconButton>
-          </Tooltip>
+          <LinksEditor />
           <Tooltip title="Add">
             <IconButton onClick={add_openDialog}>
               <AddIcon />
@@ -143,83 +86,6 @@ function Links() {
           </Tooltip>
         </div>
       </div>
-
-      <Dialog
-        open={edit_open}
-        onClose={() => edit_closeDialog(false)}
-        maxWidth="md"
-      >
-        <DialogTitle>Edit your links</DialogTitle>
-        <Box
-          component="form"
-          onSubmit={(e) => {
-            e.preventDefault();
-            edit_closeDialog(true);
-          }}
-        >
-          <DialogContent>
-            {tempLinks.map((self) => (
-              <React.Fragment key={self._id}>
-                <Box sx={{ display: "flex", alignItems: "center" }}>
-                  <TextField
-                    disabled={self._toBeDeleted}
-                    sx={{
-                      marginRight: "1rem",
-                      width: "13rem",
-                      textDecoration: self._toBeDeleted ? "line-through" : "",
-                    }}
-                    margin="dense"
-                    id="name"
-                    label="Link Name"
-                    type="text"
-                    variant="outlined"
-                    value={self.name}
-                    autoComplete="off"
-                    onChange={(e) => updateTempName(self, e.target.value)}
-                  />
-                  <TextField
-                    disabled={self._toBeDeleted}
-                    sx={{
-                      marginRight: "0.5rem",
-                      width: "27rem",
-                      textDecoration: self._toBeDeleted ? "line-through" : "",
-                    }}
-                    margin="dense"
-                    id="url"
-                    label="URL"
-                    type="text"
-                    variant="outlined"
-                    required
-                    value={self.url}
-                    autoComplete="off"
-                    onChange={(e) => updateTempURL(self, e.target.value)}
-                  />
-                  {self._toBeDeleted ? (
-                    <Tooltip title="Restore link">
-                      <IconButton onClick={() => toggleToBeDeleted(self)}>
-                        <RestoreIcon />
-                      </IconButton>
-                    </Tooltip>
-                  ) : (
-                    <Tooltip title="Delete link">
-                      <IconButton onClick={() => toggleToBeDeleted(self)}>
-                        <DeleteIcon />
-                      </IconButton>
-                    </Tooltip>
-                  )}
-                  <br />
-                </Box>
-              </React.Fragment>
-            ))}
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={() => edit_closeDialog(false)}>
-              Discard Changes
-            </Button>
-            <Button type="submit">Save</Button>
-          </DialogActions>
-        </Box>
-      </Dialog>
 
       <Dialog open={add_open} onClose={add_closeDialog} maxWidth="md">
         <DialogTitle>Add a new link</DialogTitle>
