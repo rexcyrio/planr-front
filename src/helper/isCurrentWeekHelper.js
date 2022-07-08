875fafbb072afbaf92a1cdfbd4547f86b7b93f3b
wz2k <87819279+wz2k@users.noreply.github.com>
2022-07-08 21:52:46 +0800

    feat: current week helper

---
+++ src/helper/isCurrentWeekHelper.js
@@ -0,0 +1,11 @@
+import { getMondayKey } from "../store/slices/timeSlice";
+
+export default function isCurrentWeek(mondayKey) {
+  const currentMondayKey = getMondayKey(new Date());
+  for (let i = 0; i < 3; i++) {
+    if (mondayKey[i] !== currentMondayKey[i]) {
+      return false;
+    }
+  }
+  return true;
+}
