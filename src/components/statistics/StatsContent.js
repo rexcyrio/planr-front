fc67e25f66a53a8e1b2731ec3a1d5e51469fe813
wz2k <87819279+wz2k@users.noreply.github.com>
2022-07-14 22:34:04 +0800

    feat: stats section new graphs and style

--- src/components/statistics/StatsContent.js
+++ src/components/statistics/StatsContent.js
@@ -1,14 +1,24 @@
 import React from "react";
 import BarGraphSection from "./BarGraphSection";
+import LineChartSection from "./LineChartSection";
 import PieChartSection from "./PieChartSection";
 import styles from "./StatsContent.module.css";
+import Divider from "@mui/material/Divider";
 
 function StatsContent() {
   return (
-    <div className={styles.charts}>
-      <BarGraphSection />
-      <PieChartSection />
-    </div>
+    <>
+      <div className={styles.charts}>
+        <div className={styles["bar-line-grouping"]}>
+          <BarGraphSection />
+          <Divider />
+          <LineChartSection />
+        </div>
+        <div className={styles["pie-chart-div"]}>
+          <PieChartSection />
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/statistics/TasksAmountLineChart.js
@@ -0,0 +1,118 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { convertToKey } from "../../store/slices/timeSlice";
+import { useSelector } from "react-redux";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: "Workload across weeks",
+    },
+  },
+};
+
+const labels = [
+  "3 weeks before",
+  "2 weeks before",
+  "1 week before",
+  "Current week",
+  "1 week after",
+  "2 weeks after",
+  "3 weeks after",
+];
+
+function mondayKeySwitcher(value, mondayKey, modulesWeekHours) {
+  const [dateNumber, monthNumber, yearNumber] = mondayKey;
+  const dateObject = new Date(yearNumber, monthNumber, dateNumber + 7 * value);
+  const newMondayKey = convertToKey(dateObject);
+  return newMondayKey;
+}
+
+function generateWeeksTasksDistribution(tasks, modules, mondayKey) {
+  const countObject = {};
+  const recurringTasksWeekHours = getRecurringTasksWeekHours(tasks);
+  const modulesWeekHours = getModulesWeekHours(modules);
+
+  for (let i = -3; i < 4; i++) {
+    const key = mondayKeySwitcher(i, mondayKey);
+    countObject[key] = modulesWeekHours + recurringTasksWeekHours;
+  }
+
+  for (const task of tasks) {
+    if (countObject[task.mondayKey]) {
+      countObject[task.mondayKey] += task.timeUnits / 2;
+    }
+  }
+
+  return Object.values(countObject);
+}
+
+function getModulesWeekHours(modules) {
+  let count = 0;
+
+  for (const module of modules) {
+    count += module.timeUnits / 2;
+  }
+  return count;
+}
+
+function getRecurringTasksWeekHours(tasks) {
+  let count = 0;
+
+  for (const task of tasks) {
+    if (task.mondayKey.length === 0) {
+      count += task.timeUnits / 2;
+    }
+  }
+  return count;
+}
+
+function TasksAmountLineChart() {
+  const tasks = useSelector((state) => state.tasks.data);
+  const modules = useSelector((state) => state.modules);
+  const mondayKey = useSelector((state) => state.time.mondayKey);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        fill: true,
+        label: "Number of hours",
+        data: generateWeeksTasksDistribution(tasks, modules, mondayKey),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
+  return <Line options={options} data={data} />;
+}
+
+export default TasksAmountLineChart;
