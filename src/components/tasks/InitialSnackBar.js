a8fd492df231072fb12ad580015f2c51738a22aa
rexcyrio <stefancyrio@gmail.com>
2022-07-15 11:08:38 +0800

    feat: initial snackbar shows overdue tasks

--- src/components/tasks/InitialSnackBar.js
+++ src/components/tasks/InitialSnackBar.js
@@ -10,27 +10,105 @@ function InitialSnackBar() {
   const isNewUser = useSelector((state) => state.isNewUser);
   const [open, setOpen] = useState(false);
 
+  const [color, setColor] = useState("");
+  const [numTasks, setNumTasks] = useState(0);
+
   useEffect(() => {
-    if (!isNewUser && status === FETCH_SUCCESS) {
+    if (status === FETCH_SUCCESS && !isNewUser) {
+      init();
       setOpen(true);
     }
-  }, [isNewUser, status]);
+  }, [status, isNewUser]);
 
   function handleClose() {
     setOpen(false);
   }
 
   function getText() {
-    const numberOfOutstandingTasks = tasks.filter(
-      (each) => !each.isCompleted
-    ).length;
-
-    if (numberOfOutstandingTasks === 0) {
-      return "There is no outstanding tasks";
-    } else if (numberOfOutstandingTasks === 1) {
-      return "There is 1 outstanding task";
+    if (color === "error") {
+      if (numTasks === 1) {
+        return `There is 1 overdue task`;
+      } else {
+        return `There are ${numTasks} overdue tasks`;
+      }
+    } else if (color === "warning") {
+      if (numTasks === 1) {
+        return `There is 1 task due soon`;
+      } else {
+        return `There are ${numTasks} tasks due soon`;
+      }
+    } else if (color === "info") {
+      if (numTasks === 0) {
+        return "There is no outstanding tasks";
+      } else if (numTasks === 1) {
+        return "There is 1 outstanding task";
+      } else {
+        return `There are ${numTasks} outstanding tasks`;
+      }
     } else {
-      return `There are ${numberOfOutstandingTasks} outstanding tasks`;
+      return "";
+    }
+  }
+
+  function init() {
+    const mappingTaskEpochDueTimeToCount = {};
+
+    for (const task of tasks) {
+      const { dueDate, dueTime, isCompleted } = task;
+
+      if (isCompleted) {
+        continue;
+      }
+
+      const [year, month, date] = dueDate.split("-");
+      const [hour, min] = dueTime.split(":");
+
+      // need to convert `month` back to 0-index for Date constructor to work
+      const epochDueTime = new Date(year, month - 1, date, hour, min).getTime();
+
+      if (epochDueTime in mappingTaskEpochDueTimeToCount) {
+        mappingTaskEpochDueTimeToCount[epochDueTime] += 1;
+      } else {
+        mappingTaskEpochDueTimeToCount[epochDueTime] = 1;
+      }
+    }
+
+    const epochTimeNow = Date.now();
+    const dateNow = new Date();
+
+    // if today is Monday, this Date object would be Thursday 0000H so as to
+    // encompass tasks that are due on Wednesday 2359H
+    const epochTime2DaysLater = new Date(
+      dateNow.getFullYear(),
+      dateNow.getMonth(),
+      dateNow.getDate() + 3
+    ).getTime();
+
+    let overdueTasksCount = 0;
+    let due2DaysLaterCount = 0;
+    let dueMuchLaterCount = 0;
+
+    for (const [epochDueTime, count] of Object.entries(
+      mappingTaskEpochDueTimeToCount
+    )) {
+      if (epochDueTime < epochTimeNow) {
+        overdueTasksCount += count;
+      } else if (epochDueTime < epochTime2DaysLater) {
+        due2DaysLaterCount += count;
+      } else {
+        dueMuchLaterCount += count;
+      }
+    }
+
+    if (overdueTasksCount > 0) {
+      setColor("error");
+      setNumTasks(overdueTasksCount);
+    } else if (due2DaysLaterCount > 0) {
+      setColor("warning");
+      setNumTasks(due2DaysLaterCount);
+    } else {
+      setColor("info");
+      setNumTasks(dueMuchLaterCount);
     }
   }
 
@@ -41,7 +119,7 @@ function InitialSnackBar() {
       anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
       sx={{ backgroundColor: "rgb(0, 0, 0, 0)" }}
     >
-      <Alert onClose={handleClose} severity="info" sx={{ width: "100%" }}>
+      <Alert onClose={handleClose} severity="info" color={color}>
         {getText()}
       </Alert>
     </Snackbar>
