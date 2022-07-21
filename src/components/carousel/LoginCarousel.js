8f6829a54f347c440b4b77fc9cb66b0978cc78ce
rexcyrio <stefancyrio@gmail.com>
2022-07-21 12:57:12 +0800

    fix: alt tag for images

--- src/components/carousel/LoginCarousel.js
+++ src/components/carousel/LoginCarousel.js
@@ -18,19 +18,22 @@ function LoginCarousel() {
         width="40rem"
       >
         <div>
-          <img src={importNUSMods} alt="frist" />
+          <img src={importNUSMods} alt="Import your own NUSMods timetable" />
           <p className="legend">Import your own NUSMods timetable</p>
         </div>
         <div>
-          <img src={colourCodedTimetable} alt="second" />
+          <img
+            src={colourCodedTimetable}
+            alt="Colour coding based on modules"
+          />
           <p className="legend">Colour coding based on modules</p>
         </div>
         <div>
-          <img src={dragAndDrop} alt="second" />
+          <img src={dragAndDrop} alt="Fast and easy drag and drop scheduling" />
           <p className="legend">Fast and easy drag and drop scheduling</p>
         </div>
         <div>
-          <img src={charts} alt="second" />
+          <img src={charts} alt="Analyse your timetable with dynamic charts" />
           <p className="legend">Analyse your timetable with dynamic charts</p>
         </div>
       </Carousel>
